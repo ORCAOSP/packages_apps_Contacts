467717d56dcc6b99ba199cc016f204d456bccfd5
orcaosp <orcaproject@yahoo.com>
2013-04-16 20:53:06 -0400

    intial bigfoot and orca commit

--- src/com/android/contacts/dialpad/DialpadFragment.java
+++ src/com/android/contacts/dialpad/DialpadFragment.java
@@ -329,7 +329,6 @@ public class DialpadFragment extends Fragment
         }
         mT9Flipper = (ViewSwitcher) fragmentView.findViewById(R.id.t9flipper);
         mT9Top = (LinearLayout) fragmentView.findViewById(R.id.t9topbar);
-
         PhoneNumberFormatter.setPhoneNumberFormattingTextWatcher(getActivity(), mDigits);
         // Check for the presence of the keypad
         View oneButton = fragmentView.findViewById(R.id.one);
@@ -533,9 +532,6 @@ public class DialpadFragment extends Fragment
     @Override
     public void onStart() {
         super.onStart();
-        if (isT9On()) {
-            mT9Search.refresh(mT9Callback);
-        }
         configureScreenFromIntent(getActivity().getIntent());
         setStartedFromNewIntent(false);
 
